a48e3cd3cdc1a827335d087eeae603003550a501
ashish <ashishbindra2@gmail.com>
2025-10-09 16:45:13 +0530

    testing site

---
+++ docs/DSA/stack.py
@@ -0,0 +1,16 @@
+stack = []
+
+stack.append(10)
+stack.append(20)
+stack.append(30)
+stack.append(40)
+stack.append(50)
+
+print(stack)
+
+top = stack[-1]
+
+print(top, len(stack))
+
+print(stack.pop())
+print(stack)
